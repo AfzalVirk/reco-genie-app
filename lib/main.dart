c44e5c4c722b181a739fc8bea8ba480621e26ce0
AfzalVirk <vafzal078@gmail.com>
2025-07-20 16:47:57 +0500

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          child: Text('hello'),
+          child: Text('hello world'),
         ),
       ),
     );
